978eeeea4321adda1a19959bc44b9c1bd59dc1fe
armersai <rikmersai@gmail.com>
2024-05-10 20:38:59 -0700

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -315,14 +315,17 @@
     
     <h3 bind:this={sevenTwo}>7.2 Payout</h3>
     <p>League payout is structured as follows:</p>
-    <ul>
-        <li>1st place: {dues * 8}$</li>
-        <li>2nd place: {dues * 3}$</li>
-        <li>3rd place: {dues}$</li>
-    </ul>
+    <ol>
+        <li>Playoff Standings</li>
+        <li>Weekly Top Scorer* - $10</li>
+        <li>"Bent Over" Payout** - $20</li>
+        <li>Record Against League Median* - $50</li>
+    </ol>
+    <p>*End of the regular season
+    <p>**Highest points against at the end of the regular season
     <p>In the event that a manager was fined due to late payment, their extra fee will be added to the winner's payout. Similarly, if new managers come into the league and the payout is smaller, the difference will come out of the winner's prize.</p>
     
     <h3 bind:this={sevenThree}>7.3 Raising Dues</h3>
-    <p>A ⅔ majority vote is required in order to raise league dues. In the event that a manager is no longer financially comfortable with the buyin, finding a co-manager to split the cost is reccomended.</p>
+    <p>A majority vote, 10/14 (~70%),  is required in order to raise league dues. In the event that a manager is no longer financially comfortable with the buyin, finding a co-manager to split the cost is recommended.</p>
 
 </div>
